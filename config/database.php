f0dfc6180d228666828c6b8dae80cc9003cbbe0f
JaguarJack <82664165@qq.com>
2018-11-21 13:42:06 +0800

    更新 database.php

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'cms',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => 'admin',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
@@ -29,7 +29,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => 'cms_',
+    'prefix'          => '',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
