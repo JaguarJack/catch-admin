b2e8d6ba5ed93109d0de22ba7ab573500d26d447
JaguarJack <82664165@qq.com>
2019-01-21 15:53:10 +0800

    修改配置文件

--- config/app.php
+++ config/app.php
@@ -130,8 +130,8 @@ return [
     'route_cache_option'     => [],
 
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'  => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+    'dispatch_success_tmpl'  => Env::get('root_path') . 'views' . DIRECTORY_SEPARATOR . 'admin/message.html',//Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'    => Env::get('root_path') . 'views' . DIRECTORY_SEPARATOR . 'admin/message.html',//Env::get('think_path') . 'tpl/dispatch_jump.tpl',
 
     // 异常页面的模板文件
     'exception_tmpl'         => Env::get('think_path') . 'tpl/think_exception.tpl',
