67e79bfa190c646eac819606bafc6d2168a9c9c9
wuyanwen <wuyanwen@baijiayun.com>
2019-12-12 18:53:25 +0800

    增加后台配置

--- config/catch.php
+++ config/catch.php
@@ -1,1 +1,17 @@
 <?php
+return [
+    // 忽略菜单
+    'ignore' => [
+        'route' => [
+            'index:index:index', // 首页
+            'index:index:theme', // 主题选择
+        ],
+        // 模块
+        'module' => [
+            'login',
+        ],
+    ],
+
+    // 错误提示
+    'error' => root_path('catchAdmin/index/view/')  . 'error.html',
+];
