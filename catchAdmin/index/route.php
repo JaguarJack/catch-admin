a0a62b5640e0a4d390cdfe4e415117ab201bae0f
wuyanwen <wuyanwen@baijiayun.com>
2019-12-12 18:52:44 +0800

    增加默认错误页面

--- catchAdmin/index/route.php
+++ catchAdmin/index/route.php
@@ -1,4 +1,5 @@
 <?php
 
-$router->get('/', '\catchAdmin\index\controller\index/index');
-$router->get('theme', '\catchAdmin\index\controller\index/theme');
+$router->get('/', '\catchAdmin\index\controller\index@index');
+$router->get('theme', '\catchAdmin\index\controller\index@theme');
+$router->get('dashboard', '\catchAdmin\index\controller\index@dashboard');
