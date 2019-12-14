34d75c3f4660e4c7fb5c8db24db19f49e205e949
wuyanwen <wuyanwen@baijiayun.com>
2019-12-14 18:21:29 +0800

    修改路由加载

--- extend/catcher/event/LoadModuleRoutes.php
+++ extend/catcher/event/LoadModuleRoutes.php
@@ -19,7 +19,19 @@ class LoadModuleRoutes
         $router = app(Route::class);
 
         $router->group(function () use ($router) {
-            include CatchAdmin::getRoutes();
+            foreach ($this->getRoutes() as $route) {
+                include $route;
+            }
         });
     }
+
+    protected function getRoutes()
+    {
+        return [
+            CatchAdmin::directory() . 'login' . DIRECTORY_SEPARATOR . 'route.php',
+            CatchAdmin::getRoutes(),
+        ];
+    }
+
+
 }
