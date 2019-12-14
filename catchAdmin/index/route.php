485432acfb6bf59f2a5c9b6b2f6fd30a83b8688f
wuyanwen <wuyanwen@baijiayun.com>
2019-12-14 16:56:59 +0800

    修改路由

--- catchAdmin/index/route.php
+++ catchAdmin/index/route.php
@@ -1,5 +1,5 @@
 <?php
 
-$router->get('/', '\catchAdmin\index\controller\index@index');
-$router->get('theme', '\catchAdmin\index\controller\index@theme');
-$router->get('dashboard', '\catchAdmin\index\controller\index@dashboard');
+$router->get('/', '\catchAdmin\index\controller\Index@index');
+$router->get('theme', '\catchAdmin\index\controller\Index@theme');
+$router->get('dashboard', '\catchAdmin\index\controller\Index@dashboard');
