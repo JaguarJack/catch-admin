4a8a0b90e83445093fbf9a8ae6dc86fe604e40f7
wuyanwen <wuyanwen@baijiayun.com>
2020-01-06 17:42:51 +0800

    delete view

---
+++ public/dist/js/chunk-2d0c4fb8.0c1495ee.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4fb8"],{"3cc5":function(n,c,u){"use strict";u.r(c);var l,e,o=u("2877"),p={},s=Object(o["a"])(p,l,e,!1,null,null,null);c["default"]=s.exports}}]);
---
+++ public/dist/js/fail.24b6b389.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["fail"],{cc89:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("exception-page",{attrs:{type:"404"}})},a=[],o=n("2af9"),p={components:{ExceptionPage:o["c"]}},s=p,i=n("2877"),r=Object(i["a"])(s,c,a,!1,null,"1f038743",null);t["default"]=r.exports}}]);
