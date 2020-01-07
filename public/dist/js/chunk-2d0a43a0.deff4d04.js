a03f3c3699b7a7a2c350fa18390b54ba2b75f2e5
wuyanwen <wuyanwen@baijiayun.com>
2020-01-07 21:52:38 +0800

    删除文件

--- public/dist/js/chunk-2d0a43a0.deff4d04.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a43a0"],{"0615":function(n,u,a){"use strict";a.r(u);var l,c,e=a("2877"),o={},p=Object(e["a"])(o,l,c,!1,null,null,null);u["default"]=p.exports}}]);
