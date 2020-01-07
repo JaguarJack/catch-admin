ba91154ee823ded0ecc508a18dbd29fa213d5241
wuyanwen <wuyanwen@baijiayun.com>
2020-01-07 19:00:17 +0800

    增加auth配置

--- config/catch.php
+++ config/catch.php
@@ -10,4 +10,27 @@ return [
      * set error page
      */
     'error' => root_path('catch/index/view/')  . 'error.html',
+
+
+  'auth' => [
+      // 默认
+      'default' => [
+        'guard' => 'admin',
+      ],
+      // 门面设置
+      'guards' => [
+        // admin 认证
+        'admin' => [
+          'driver' => 'jwt',
+          'provider' => 'admin_users',
+        ],
+      ],
+      // 服务提供
+      'providers' => [
+        'admin_users' => [
+          'driver' => 'orm',
+          'model' =>  \catchAdmin\user\model\Users::class,
+        ],
+      ],
+  ],
 ];
