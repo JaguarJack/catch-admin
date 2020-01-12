7c09bcf0cfe5ba1f1cc694e72f0a51d039177320
yanwenwu <njphper@gmail.com>
2020-01-12 09:32:32 +0800

    新增用户岗位关联表

---
+++ catch/permissions/database/migrations/20200112003534_user_relate_job.php
@@ -0,0 +1,36 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class UserRelateJob extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table  =  $this->table('user_has_jobs',['engine'=>'Innodb', 'comment' => '用户角色表', 'signed' => false]);
+        $table->addColumn('uid', 'integer',['comment'=>'用户ID', 'signed' => false])
+            ->addColumn('job_id', 'integer', ['comment'=>'岗位ID', 'signed' => false])
+            ->create();
+    }
+}
