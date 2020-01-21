ef545b8732be243892baaa58b0a29f2888c636c2
wuyanwen <wuyanwen@baijiayun.com>
2020-01-21 16:59:40 +0800

    新增stubs

---
+++ extend/catcher/command/MigrateRollbackCommand.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @filename  MigrateRollbackCommand.php
+ * @createdAt 2020/1/20
+ * @project  https://github.com/yanwenwu/catch-admin
+ * @document http://doc.catchadmin.com
+ * @author   JaguarJack <njphper@gmail.com>
+ * @copyright By CatchAdmin
+ * @license  https://github.com/yanwenwu/catch-admin/blob/master/LICENSE.txt
+ */
