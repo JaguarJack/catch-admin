714837c8dfd65dee4558f351049c42252d29dcfb
yanwenwu <njphper@gmail.com>
2020-01-25 20:06:36 +0800

    新增cloud配置

--- config/filesystem.php
+++ config/filesystem.php
@@ -22,5 +22,38 @@ return [
             'visibility' => 'public',
         ],
         // 更多的磁盘配置信息
-    ],
+        'qiniu' => [
+            'type'       => 'qiniu',
+            'access_key' => 'iLOE193LsBtlY4LZSN6cMIec9FTmMDRXPoFnBAec',
+            'secret_key' => '2w7bQ6dw_HA2mfZHxXyzROxXach_9-6kavgJ9aHl',
+            'bucket'     => 'assets',
+            'domain'     => 'assets.njphper.com',
+        ],
+        'oss' => [
+            'type'   => 'oss',
+            'prefix' => '',
+            'access_key' => 'XbBP7l7B8ZrXOkDk',
+            'secret_key' => 'EhNxzuIMZJsvpjWXSyvsSgcCML5In2',
+            'end_point'  => 'https://oss-cn-beijing.aliyuncs.com', // ssl：https://iidestiny.com
+            'bucket'     => 'jaguarjack-test',
+            'is_cname'   =>  false
+        ],
+        // 腾讯云配置
+        'qcloud' => [
+            'type'        => 'qcloud',
+            'region'      => 'ap-chengdu',
+            'credentials' => [
+                'appId'      => '1252879105', // 域名中数字部分
+                'secretId'   => 'AKIDVpqhjprOp0BNL2EhhRurb7QZjspWmoiN',
+                'secretKey'  => 'ojV3OuEKCohjWq0yhBPZWEoFTTYN5c5w',
+            ],
+            'bucket'          => 'jaguarjack-1252879105',
+            'timeout'         => 60,
+            'connect_timeout' => 60,
+            'cdn'             => '',
+            'scheme'          => 'https',
+            'read_from_cdn'   => false,
+        ]
+    ]
+
 ];
