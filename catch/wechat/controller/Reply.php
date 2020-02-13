98f2e4bc31e4ab7ef35bb4baab91e6297ddf1222
wuyanwen <wuyanwen@baijiayun.com>
2020-02-13 19:55:01 +0800

    修改异常错误信息

---
+++ catch/wechat/controller/Reply.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @filename  Reply.php
+ * @createdAt 2020/2/13
+ * @project  https://github.com/yanwenwu/catch-admin
+ * @document http://doc.catchadmin.com
+ * @author   JaguarJack <njphper@gmail.com>
+ * @copyright By CatchAdmin
+ * @license  https://github.com/yanwenwu/catch-admin/blob/master/LICENSE.txt
+ */
--- config/app.php
+++ config/app.php
@@ -32,5 +32,5 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => false,
+    'show_error_msg'   => true,
 ];
