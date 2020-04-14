b47ff98bb6569ef57c8de242d4de7624b3f03556
JaguarJack <njphper@gmail.com>
2020-04-14 19:16:28 +0800

    新增权限模型

--- catch/permissions/model/Permissions.php
+++ catch/permissions/model/Permissions.php
@@ -11,21 +11,24 @@ class Permissions extends CatchModel
     protected $name = 'permissions';
     
     protected $field = [
-            'id', // 
-			'permission_name', // 菜单名称
-			'parent_id', // 父级ID
-      'icon',
-      'creator_id',
-      'module', // 模块
-			'route', // 路由
-      'method', // 请求方法
-			'permission_mark', // 权限标识
-			'type', // 1 菜单 2 按钮
-			'sort', // 排序字段
-			'created_at', // 创建时间
-			'updated_at', // 更新时间
-			'deleted_at', // 删除状态，null 未删除 timestamp 已删除
-			   
+        'id', //
+        'permission_name', // 菜单名称
+        'parent_id', // 父级ID
+        'icon',
+        'component', // 组件
+        'redirect',
+        'keep_alive',
+        'hide_children_in_menu',
+        'creator_id',
+        'module', // 模块
+        'route', // 路由
+        'method', // 请求方法
+        'permission_mark', // 权限标识
+        'type', // 1 菜单 2 按钮
+        'sort', // 排序字段
+        'created_at', // 创建时间
+        'updated_at', // 更新时间
+        'deleted_at', // 删除状态，null 未删除 timestamp 已删除
     ];
 
     public const MENU_TYPE = 1;
