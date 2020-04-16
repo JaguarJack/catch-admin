f1f3df2bc8876465f98794b7653d972c34c558e3
JaguarJack <njphper@gmail.com>
2020-04-17 06:47:19 +0800

    重写路由

--- catch/system/route.php
+++ catch/system/route.php
@@ -1,11 +1,9 @@
 <?php
 // 登录日志
 $router->get('log/login', '\catchAdmin\system\controller\LoginLog@list');
-$router->get('loginLog/index', '\catchAdmin\system\controller\LoginLog@index');
 $router->delete('loginLog/empty', '\catchAdmin\system\controller\LoginLog@empty');
 // 操作日志
 $router->get('log/operate', '\catchAdmin\system\controller\OperateLog@list');
-$router->get('operateLog/index', '\catchAdmin\system\controller\OperateLog@index');
 $router->delete('operateLog/empty', '\catchAdmin\system\controller\OperateLog@empty');
 
 // 数据字典
