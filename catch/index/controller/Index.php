57abdd4fd961c538c707e9ef26b07f53f548f2f4
JaguarJack <njphper@gmail.com>
2020-04-22 06:07:53 +0800

    删除冗余代码

--- catch/index/controller/Index.php
+++ catch/index/controller/Index.php
@@ -1,51 +1,11 @@
 <?php
 namespace catchAdmin\index\controller;
 
-use catchAdmin\permissions\model\Permissions;
-use catchAdmin\user\Auth;
 use catcher\base\CatchController;
-use catcher\CatchAuth;
-use catcher\Tree;
 use think\facade\Db;
 
 class Index extends CatchController
 {
-    /**
-     *
-     * @time 2019年12月11日
-     * @return string
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\db\exception\DataNotFoundException
-     */
-    public function index(): string
-    {
-        $user = (new CatchAuth())->user();
-
-        $permissionIds = $user->getPermissionsBy();
-
-        $menus = Permissions::whereIn('id', $permissionIds)
-                    ->where('type', Permissions::MENU_TYPE)
-                    ->field(['id', 'parent_id', 'permission_name', 'route'])
-                    ->select()->toArray();
-
-        return $this->fetch([
-            'menus' => Tree::done($menus),
-            'username' => $user->username,
-        ]);
-    }
-
-    /**
-     *
-     * @time 2019年12月11日
-     * @throws \Exception
-     * @return string
-     */
-    public function theme(): string
-    {
-        return $this->fetch();
-    }
-
     /**
      *
      * @time 2019年12月12日
