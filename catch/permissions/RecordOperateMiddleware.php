98c99e05f31630929c4285b714c0ce99c3bf97e1
JaguarJack <njphper@gmail.com>
2020-05-06 17:40:51 +0800

    分离日志操作权限

--- catch/permissions/RecordOperateMiddleware.php
+++ catch/permissions/RecordOperateMiddleware.php
@@ -9,7 +9,7 @@ use catcher\exceptions\PermissionForbiddenException;
 use think\facade\Cache;
 use catcher\Utils;
 
-class PermissionsMiddleware
+class RecordOperateMiddleware
 {
     /**
      *
@@ -26,37 +26,15 @@ class PermissionsMiddleware
     {
         $rule = $request->rule()->getName();
 
-        if (!$rule) {
-            return $next($request);
-        }
-
         // 模块忽略
         [$module, $controller, $action] = Utils::parseRule($rule);
-        // toad
-        if (in_array($module, $this->ignoreModule())) {
-            return $next($request);
-        }
-        // 用户未登录
-        $user = $request->user();
-        if (!$user) {
-            throw new PermissionForbiddenException('Login is invalid', Code::LOST_LOGIN);
-        }
-        // 超级管理员
-        if ($request->user()->id === config('catch.permissions.super_admin_id')) {
-            return $next($request);
-        }
-        // Get 请求
-        if ($request->isGet() && config('catch.permissions.is_allow_get')) {
-            return $next($request);
-        }
-        // 判断权限
-        $permission = property_exists($request, 'permission') ? $request->permission :
-                        $this->getPermission($module, $controller, $action);
 
-        if (!$permission || !in_array($permission->id, Cache::get(CatchCacheKeys::USER_PERMISSIONS . $user->id))) {
-          throw new PermissionForbiddenException();
-        }
+        $permission = $this->getPermission($module, $controller, $action);
 
+        $this->operateEvent($request->user()->id, $permission);
+
+        // 将权限带入
+        $request->permission  = $permission;
         return $next($request);
     }
 
@@ -77,17 +55,6 @@ class PermissionsMiddleware
         $permissionMark = sprintf('%s@%s', $controllerName, $action);
 
         return Permissions::where('module', $module)->where('permission_mark', $permissionMark)->find();
-    }
-
-    /**
-     * 忽略模块
-     *
-     * @time 2020年04月16日
-     * @return array
-     */
-    protected function ignoreModule()
-    {
-        return ['login'];
     }
 
     /**
