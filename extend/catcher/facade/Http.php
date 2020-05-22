b7d951bdb78dbe0ec2ba9e4aa9ef9586bc60efd5
JaguarJack <njphper@gmail.com>
2020-05-22 14:08:35 +0800

    新增facade方法注释，以便用户访问

--- extend/catcher/facade/Http.php
+++ extend/catcher/facade/Http.php
@@ -3,6 +3,22 @@ namespace catcher\facade;
 
 use think\Facade;
 
+/**
+ * @method static \catcher\library\client\Http headers(array $headers)
+ * @method static \catcher\library\client\Http body($body)
+ * @method static \catcher\library\client\Http json(array $data)
+ * @method static \catcher\library\client\Http query(array $query)
+ * @method static \catcher\library\client\Http form(array $params)
+ * @method static \catcher\library\client\Http timeout($timeout)
+ * @method static \catcher\library\client\Http get(string $url)
+ * @method static \catcher\library\client\Http post(string $url)
+ * @method static \catcher\library\client\Http put(string $url)
+ * @method static \catcher\library\client\Http delete(string $url)
+ * @method static \catcher\library\client\Http token(string $token)
+ * @method static \catcher\library\client\Http attach($name, $resource, $filename)
+ *
+ * @time 2020年05月22日
+ */
 class Http extends Facade
 {
     protected static function getFacadeClass()
