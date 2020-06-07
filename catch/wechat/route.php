ec5a5a840222307d394e9938e114e613f2ca6525
JaguarJack <njphper@gmail.com>
2020-06-07 14:22:41 +0800

    新增微信模块

---
+++ catch/wechat/route.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @filename route.php
+ * @date     2020/6/7
+ * @project  https://github.com/yanwenwu/catch-admin
+ * @document http://doc.catchadmin.com
+ * @author   JaguarJack <njphper@gmail.com>
+ * @copyright By CatchAdmin
+ * @license  https://github.com/yanwenwu/catch-admin/blob/master/LICENSE.txt
+ */
+$router->resource('official/users', '\catchAdmin\wechat\controller\Users');
---
+++ extend/catcher/library/WeChat.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @filename WeChat.php
+ * @date     2020/6/7
+ * @project  https://github.com/yanwenwu/catch-admin
+ * @document http://doc.catchadmin.com
+ * @author   JaguarJack <njphper@gmail.com>
+ * @copyright By CatchAdmin
+ * @license  https://github.com/yanwenwu/catch-admin/blob/master/LICENSE.txt
+ */
