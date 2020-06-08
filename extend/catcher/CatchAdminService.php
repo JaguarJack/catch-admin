5609d4fd0989ff6359b43ccc3b93bc6d0378366f
JaguarJack <njphper@gmail.com>
2020-06-08 08:54:50 +0800

    fixed

--- extend/catcher/CatchAdminService.php
+++ extend/catcher/CatchAdminService.php
@@ -1,6 +1,7 @@
 <?php
 namespace catcher;
 
+use catcher\command\TestCommand;
 use catcher\command\Tools\BackupCommand;
 use catcher\command\Tools\CompressPackageCommand;
 use catcher\command\CreateModuleCommand;
@@ -13,6 +14,7 @@ use catcher\command\ModuleCacheCommand;
 use catcher\command\SeedRunCommand;
 use catcher\command\Tools\ExportDataCommand;
 use catcher\command\Tools\MakeMenuCommand;
+use catcher\command\worker\ExcelTaskCommand;
 use catcher\command\worker\WsWorkerCommand;
 use think\exception\Handle;
 use think\facade\Validate;
@@ -68,6 +70,8 @@ class CatchAdminService extends Service
             WsWorkerCommand::class,
             ExportDataCommand::class,
             MakeMenuCommand::class,
+            TestCommand::class,
+            ExcelTaskCommand::class,
         ]);
     }
     /**
