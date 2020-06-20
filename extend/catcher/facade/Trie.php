97724b04df38b91fde7b83c74cce17e39c54382a
JaguarJack <njphper@gmail.com>
2020-06-20 16:57:37 +0800

    新增catchadmin模块发现

--- extend/catcher/facade/Trie.php
+++ extend/catcher/facade/Trie.php
@@ -1,1 +1,18 @@
 <?php
+namespace catcher\facade;
+
+use think\Facade;
+
+/**
+ * @method static \catcher\library\Trie add(string $word)
+ * @method static \catcher\library\Trie filter(string $content)
+ *
+ * @time 2020年05月22日
+ */
+class Trie extends Facade
+{
+    protected static function getFacadeClass()
+    {
+        return \catcher\library\Trie::class;
+    }
+}
---
+++ extend/catcher/library/ProgressBar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @filename  ProgressBar.php
+ * @createdAt 2020/6/20
+ * @project  https://github.com/yanwenwu/catch-admin
+ * @document http://doc.catchadmin.com
+ * @author   JaguarJack <njphper@gmail.com>
+ * @copyright By CatchAdmin
+ * @license  https://github.com/yanwenwu/catch-admin/blob/master/LICENSE.txt
+ */
