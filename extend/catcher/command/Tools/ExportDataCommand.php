4dc2978d614121cd18e67009c40b6a5499b3d967
JaguarJack <njphper@gmail.com>
2020-06-25 08:36:29 +0800

    新增模块权限导出

--- extend/catcher/command/Tools/ExportDataCommand.php
+++ extend/catcher/command/Tools/ExportDataCommand.php
@@ -3,6 +3,7 @@ declare (strict_types = 1);
 
 namespace catcher\command\Tools;
 
+use catcher\CatchAdmin;
 use catcher\facade\Http;
 use catcher\Tree;
 use catcher\Utils;
@@ -23,24 +24,43 @@ class ExportDataCommand extends Command
         $this->setName('export')
             ->addArgument('table', Argument::REQUIRED, 'export tables')
             ->addOption('pid', '-p', Option::VALUE_REQUIRED, 'parent level name')
+            ->addOption('module', '-m', Option::VALUE_REQUIRED, 'module name')
             ->setDescription('Just for catchAdmin export data');
     }
 
     protected function execute(Input $input, Output $output)
     {
         $table = Utils::tablePrefix() . $input->getArgument('table');
-
         $parent = $input->getOption('pid');
+        $module = $input->getOption('module');
 
-        $data = Db::name($table)->where('deleted_at', 0)->select()->toArray();
+        if ($module) {
+            $data = Db::name($table)->where('deleted_at', 0)
+                                    ->where('module', $module)
+                                    ->select()
+                                    ->toArray();
+        } else {
+            $data = Db::name($table)->where('deleted_at', 0)->select()->toArray();
+        }
 
         if ($parent) {
             $data = Tree::done($data, 0, $parent);
         }
 
-        file_put_contents(root_path() . DIRECTORY_SEPARATOR . $table . '.php', "<?php\r\n return " . var_export($data, true) . ';');
-
+        if ($module) {
+            $data = 'return ' . var_export($data, true) . ';';
+            $this->exportSeed($data, $module);
+        } else {
+            file_put_contents(root_path() . DIRECTORY_SEPARATOR . $table . '.php', "<?php\r\n return " . var_export($data, true) . ';');
+        }
         $output->info('succeed!');
+    }
+
+    protected function exportSeed($data, $module)
+    {
+        $stub = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . 'permissionSeed.stub');
+
+        file_put_contents(CatchAdmin::moduleSeedsDirectory($module) . 'permissionSeed.php', str_replace('{DATA}', $data, $stub));
     }
 }
 
