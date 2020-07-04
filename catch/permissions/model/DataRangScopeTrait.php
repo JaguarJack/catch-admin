c2da2e5806769f161c66bc9f7a054ae1abe7af40
JaguarJack <njphper@gmail.com>
2020-07-04 14:28:59 +0800

    修复数据权限 bug

--- catch/permissions/model/DataRangScopeTrait.php
+++ catch/permissions/model/DataRangScopeTrait.php
@@ -39,14 +39,14 @@ trait DataRangScopeTrait
                     break;
                 case Roles::SELF_CHOOSE:
                     $departmentIds = array_merge(array_column($role->getDepartments()->toArray(), 'id'));
-                    $userIds = array_merge($userIds, Users::getUserIdsByDepartmentIds($departmentIds));
+                    $userIds = array_merge($userIds, $this->getUserIdsByDepartmentId($departmentIds));
                     break;
                 case Roles::SELF_DATA:
                     $userIds[] = $user->id;
                     break;
                 case Roles::DEPARTMENT_DOWN_DATA:
                 case Roles::DEPARTMENT_DATA:
-                    $userIds = array_merge($userIds, Users::getUserIdsByDepartmentIds([$user->department_id]));
+                    $userIds = array_merge($userIds, $this->getUserIdsByDepartmentId([$user->department_id]));
                     break;
                 default:
                     break;
@@ -59,5 +59,17 @@ trait DataRangScopeTrait
         }
 
         return $userIds;
+    }
+
+    /**
+     * 获取UserID
+     *
+     * @time 2020年07月04日
+     * @param $id
+     * @return array
+     */
+    protected function getUserIdsByDepartmentId(array $id)
+    {
+        return Users::whereIn('department_id', $id)->column('id');
     }
 }
