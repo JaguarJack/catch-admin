3ab7e207c9c30b6bffc48229b38220b6e95f2a0a
JaguarJack <njphper@gmail.com>
2020-07-26 08:28:54 +0800

    update:修改attachments URL 路径地址

---
+++ catch/system/database/migrations/20200726001810_update_attachments_url.php
@@ -0,0 +1,38 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class UpdateAttachmentsUrl extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        if ($this->hasTable('attachments')) {
+            $table = $this->table('attachments');
+
+            $table->changeColumn('url', 'string', ['default'=> '', 'limit' => 255, 'comment'=>'资源地址'])
+                ->update();
+        }
+    }
+}
