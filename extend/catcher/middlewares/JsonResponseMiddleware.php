160e506bfee8864b9e5864c0af54c0debaeef29b
JaguarJack <njphper@gmail.com>
2020-08-27 16:19:06 +0800

    add:优雅的返回Json数据

--- extend/catcher/middlewares/JsonResponseMiddleware.php
+++ extend/catcher/middlewares/JsonResponseMiddleware.php
@@ -8,3 +8,19 @@
 // +----------------------------------------------------------------------
 // | Author: JaguarJack [ njphper@gmail.com ]
 // +----------------------------------------------------------------------
+namespace catcher\middlewares;
+
+use think\Middleware;
+use think\Request;
+
+class JsonResponseMiddleware extends Middleware
+{
+    public function handle(Request $request, \Closure $next)
+    {
+        $server = $request->server();
+        $server['HTTP_ACCEPT'] = 'application/json';
+        $request->withServer($server);
+
+        return $next($request);
+    }
+}
