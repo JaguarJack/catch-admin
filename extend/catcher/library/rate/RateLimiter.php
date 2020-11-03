47c33f754bb07e3892620fa2e71fdddedfd7580b
@小小只^v^ <soinco@qq.com>
2020-11-03 17:25:07 +0800

    fiexd: 移除手误多敲的字符

--- extend/catcher/library/rate/RateLimiter.php
+++ extend/catcher/library/rate/RateLimiter.php
@@ -1,5 +1,20 @@
 <?php
 // +----------------------------------------------------------------------
+declare(strict_types=1);
+
+/*
+ * @Application ：interfaceSkeleton
+ * @Version     : 1.0.0
+ * @Author      : @小小只 <soinco@qq.com>
+ * @Since       : 2020-11-03 17:20:50
+ * @LastAuthor  : @小小只 <soinco@qq.com>
+ * @lastTime    : 2020-11-03 17:24:26
+ * @Github      : https://github.com/littlezo
+ * @Copyright   : 2017-2020 @小小只
+ * @联系方式    : QQ:970055999  WeChat: littlezov email: soinco@qq.com
+ * @版权声明    : @小小只 拥有所有权利,未取得授权禁止任何形式的代码拷贝、派生行为 如需引用 需取得所有者授权方可
+ * @FilePath    : \\extend\\catcher\\library\\rate\\RateLimiter.php
+ */
 // | CatchAdmin [Just Like ～ ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2017~2020 http://catchadmin.com All rights reserved.
@@ -80,7 +95,7 @@ class RateLimiter
      *
      *
      * @time 2020年07月02日
-d     * @return void
+     * @return void
      */
     protected function addTokens()
     {
@@ -154,7 +169,7 @@ d     * @return void
     {
         $currentTime = \time();
 
-        $lastAddTokenTime = $this->getRedis()->get($this->key. $this->addTokenTimeKey);
+        $lastAddTokenTime = $this->getRedis()->get($this->key . $this->addTokenTimeKey);
 
         // 如果是满的 则不添加
         if ($this->tokens() == $this->capacity) {
@@ -172,6 +187,6 @@ d     * @return void
      */
     protected function rememberAddTokenTime()
     {
-        $this->getRedis()->set($this->key. $this->addTokenTimeKey, time());
+        $this->getRedis()->set($this->key . $this->addTokenTimeKey, time());
     }
 }
