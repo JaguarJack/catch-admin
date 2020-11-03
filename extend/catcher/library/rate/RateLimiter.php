9e65f7220a58a785c6cbce80df60739dd265b1f3
@小小只^v^ <soinco@qq.com>
2020-11-03 17:27:09 +0800

    fiexd: 移除手误多敲的字符

--- extend/catcher/library/rate/RateLimiter.php
+++ extend/catcher/library/rate/RateLimiter.php
@@ -8,11 +8,10 @@ declare(strict_types=1);
  * @Author      : @小小只 <soinco@qq.com>
  * @Since       : 2020-11-03 17:20:50
  * @LastAuthor  : @小小只 <soinco@qq.com>
- * @lastTime    : 2020-11-03 17:24:26
+ * @lastTime    : 2020-11-03 17:26:56
  * @Github      : https://github.com/littlezo
  * @Copyright   : 2017-2020 @小小只
  * @联系方式    : QQ:970055999  WeChat: littlezov email: soinco@qq.com
- * @版权声明    : @小小只 拥有所有权利,未取得授权禁止任何形式的代码拷贝、派生行为 如需引用 需取得所有者授权方可
  * @FilePath    : \\extend\\catcher\\library\\rate\\RateLimiter.php
  */
 // | CatchAdmin [Just Like ～ ]
