85f4fc0df8a7b79f39dee6881a3799e2c7f1e131
JaguarJack <njphper@gmail.com>
2020-11-21 20:07:36 +0800

    update:个人信息校验邮箱唯一性

--- catch/permissions/request/ProfileRequest.php
+++ catch/permissions/request/ProfileRequest.php
@@ -4,15 +4,14 @@ namespace catchAdmin\permissions\request;
 use catchAdmin\permissions\model\Users;
 use catcher\base\CatchRequest;
 
-class UpdateRequest extends CatchRequest
+class ProfileRequest extends CatchRequest
 {
     protected function rules(): array
     {
         // TODO: Implement rules() method.
         return [
             'username|用户名' => 'require|max:20',
-            'password|密码' => 'sometimes|min:5|max:12',
-            'email|邮箱'    => 'require|email|unique:'.Users::class,
+            'email|邮箱'    => 'require|email|unique:'.Users::class . ',email,' . $this->user()->id,
         ];
     }
 }
