210079819987fc13b1678fb797aca7bfd2744596
@小小只^v^ <soinco@qq.com>
2020-11-30 22:08:43 +0800

    移除无用文件

--- app/common.php
+++ app/common.php
@@ -1,2 +1,12 @@
 <?php
 // 应用公共文件
+if (!function_exists('request')) {
+    /**
+     * 获取当前Request对象实例
+     * @return Request
+     */
+    function request(): \app\Request
+    {
+        return app('request');
+    }
+}
