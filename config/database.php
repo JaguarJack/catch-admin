cff7b38058012a9d38603c220019f82c4e85eded
JaguarJack <njphper@gmail.com>
2021-01-17 11:17:37 +0800

    update:开启短线重连

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             // 是否严格检查字段是否存在
             'fields_strict'     => true,
             // 是否需要断线重连
-            'break_reconnect'   => false,
+            'break_reconnect'   => true,
             // 监听SQL
             'trigger_sql'       => true,
             // 开启字段缓存
