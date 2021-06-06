22ad849900923f698177ed9e1b636d54199eaa99
uctoo <admin@uctoo.com>
2021-06-06 15:57:50 +0800

    add create:seed command

--- extend/catcher/Utils.php
+++ extend/catcher/Utils.php
@@ -293,4 +293,32 @@ class Utils
             }
         }
     }
+
+    /**
+     * 下划线转驼峰
+     * 思路:
+     * step1.原字符串转小写,原字符串中的分隔符用空格替换,在字符串开头加上分隔符
+     * step2.将字符串中每个单词的首字母转换为大写,再去空格,去字符串首部附加的分隔符.
+     * @param $uncamelized_words
+     * @param string $separator
+     * @return string
+     */
+    public static function camelize($uncamelized_words,$separator='_')
+    {
+        $uncamelized_words = $separator. str_replace($separator, " ", strtolower($uncamelized_words));
+        return ltrim(str_replace(" ", "", ucwords($uncamelized_words)), $separator );
+    }
+
+
+    /**
+     * 驼峰命名转下划线命名
+     * 思路:
+     * @param $camelCaps
+     * @param string $separator
+     * @return string
+     */
+    public static function uncamelize($camelCaps,$separator='_')
+    {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
+    }
 }
