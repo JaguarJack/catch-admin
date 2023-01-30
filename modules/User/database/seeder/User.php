97acb5711fae15d7437f2f8fe2b04097c72d93dd
JaguarJack <njphper@gmail.com>
2023-01-30 18:31:11 +0800

    feat: user seeder

---
+++ modules/User/database/seeder/User.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Modules\User\Models\User;
+
+return new class extends Seeder
+{
+    /**
+     * Run the seeder.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $user = new User([
+            'username' => 'catchadmin',
+
+            'email' => 'catch@admin.com',
+
+            'password' => bcrypt('catchadmin'),
+
+            'creator_id' => 1,
+
+            'department_id' => 0
+        ]);
+
+        $user->save();
+    }
+};
