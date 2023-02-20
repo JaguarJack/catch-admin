cb5e15fde4c5b793099476628166372d9d38c94b
JaguarJack <njphper@gmail.com>
2023-02-20 16:24:08 +0800

    chore: update route

--- modules/User/routes/route.php
+++ modules/User/routes/route.php
@@ -6,7 +6,7 @@ use Modules\User\Http\Controllers\UserController;
 
 // login route
 Route::post('login', [AuthController::class, 'login'])->withoutMiddleware(config('catch.route.middlewares'));
-Route::post('logout', [AuthController::class, 'logout']);
+Route::post('logout', [AuthController::class, 'logout'])->withoutMiddleware(config('catch.route.middlewares'));
 
 // users route
 Route::apiResource('users', UserController::class);
