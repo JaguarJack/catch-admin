23348b3a6bb86ee53e9a5e1e882cae07de9e69fd
JaguarJack <njphper@gmail.com>
2023-04-28 10:58:52 +0800

    chore: 调整样式

--- resources/admin/layout/components/content.vue
+++ resources/admin/layout/components/content.vue
@@ -5,12 +5,12 @@
     <!-- Tag view -->
     <!--<div class=""></div>-->
     <!-- Container -->
-    <div class="p-1 sm:p-2 max-w-full h-screen overflow-auto sm:overflow-x-hidden">
+    <div class="p-1 sm:p-3 max-w-full h-screen overflow-auto sm:overflow-x-hidden">
         <div class="min-h-[calc(100vh-8rem)]">
             <router-view />
         </div>
         <div class="w-full text-center text-gray-400 h-4 leading-10">
-            <el-link href="https://catchadmin.com/">CatchAdmin 管理系统 </el-link> @copyright 2018 ~ {{ year }}
+            <el-link href="https://catchadmin.com/" target="_blank">CatchAdmin 管理系统 </el-link> @copyright 2018 ~ {{ year }}
         </div>
     </div>
   </div>
