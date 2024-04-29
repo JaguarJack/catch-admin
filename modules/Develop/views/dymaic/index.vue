99a4c0fd6a75d29d316e8dce32afd2d2e9533421
JaguarJack <82664165@qq.com>
2024-04-29 13:04:32 +0800

    chore:调整样式

--- modules/Develop/views/dymaic/index.vue
+++ modules/Develop/views/dymaic/index.vue
@@ -1,37 +1,9 @@
 <template>
     <div>
         <catch-table
-            :columns="[
-        {
-          label: '角色名称',
-          prop: 'role_name'
-        },
-        {
-          label: '角色标识',
-          prop: 'description'
-        },
-        {
-          label: '角色描述',
-          prop: 'role_name'
-        },
-        {
-          label: '创建时间',
-          prop: 'created_at'
-        },
-        {
-          type: 'operate',
-          label: '操作',
-          width: 200
-        }
-      ]"
+            :columns="columns"
             :api="api"
-            :search-form="[
-        {
-          type: 'input',
-          label: '角色名称',
-          name: 'role_name'
-        }
-      ]"
+            :search-form="search"
             row-key="id"
             :pagination="false"
         >
@@ -44,10 +16,38 @@
 
 <script lang="ts" setup>
 // @ts-nocheck
-// import Create from './form/create.vue'
 import Create from './create.vue'
 
 const api = 'permissions/roles'
-
+const columns = [
+    {
+        label: '角色名称',
+        prop: 'role_name'
+    },
+    {
+        label: '角色标识',
+        prop: 'description'
+    },
+    {
+        label: '角色描述',
+        prop: 'role_name'
+    },
+    {
+        label: '创建时间',
+        prop: 'created_at'
+    },
+    {
+        type: 'operate',
+        label: '操作',
+        width: 200
+    }
+];
+const search = [
+    {
+        type: 'input',
+        label: '角色名称',
+        name: 'role_name'
+    }
+]
 </script>
 
--- modules/Develop/views/generate/components/structure.vue
+++ modules/Develop/views/generate/components/structure.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <el-table :data="structures">
-      <el-table-column prop="field" :label="$t('generate.schema.structure.field_name.name')" width="100px" />
-      <el-table-column prop="label" :label="$t('generate.schema.structure.form_label')" width="150px">
+      <el-table-column prop="field" :label="$t('generate.schema.structure.field_name.name')"  />
+      <el-table-column prop="label" :label="$t('generate.schema.structure.form_label')">
         <template #default="scope">
           <el-input v-model="scope.row.label" />
         </template>
       </el-table-column>
-      <el-table-column prop="label" :label="$t('generate.schema.structure.form_component')" width="110px">
+      <el-table-column prop="label" :label="$t('generate.schema.structure.form_component')" >
         <template #default="scope">
           <el-select v-model="scope.row.form_component" class="w-full" filterable>
             <el-option v-for="component in formComponents" :key="component" :label="component" :value="component" />
@@ -29,14 +29,14 @@
           <el-switch v-model="scope.row.search" inline-prompt :active-text="$t('system.yes')" :inactive-text="$t('system.no')" width="45px" />
         </template>
       </el-table-column>
-      <el-table-column prop="search_op" :label="$t('generate.schema.structure.search_op.name')" width="150px">
+      <el-table-column prop="search_op" :label="$t('generate.schema.structure.search_op.name')" >
         <template #default="scope">
           <el-select v-model="scope.row.search_op" :placeholder="$t('generate.schema.structure.search_op.placeholder')" class="w-full">
             <el-option v-for="op in operates" :key="op" :label="op" :value="op" />
           </el-select>
         </template>
       </el-table-column>
-      <el-table-column prop="validates" :label="$t('generate.schema.structure.rules.name')" width="250px">
+      <el-table-column prop="validates" :label="$t('generate.schema.structure.rules.name')">
         <template #default="scope">
           <el-select v-model="scope.row.validates" :placeholder="$t('generate.schema.structure.rules.placeholder')" multiple filterable allow-create clearable class="w-full">
             <el-option v-for="validate in validates" :key="validate" :label="validate" :value="validate" />
@@ -44,7 +44,7 @@
         </template>
       </el-table-column>
       <!--<el-table-column prop="comment" label="注释" />-->
-      <el-table-column prop="id" :label="$t('generate.schema.structure.operate')" width="120px">
+      <el-table-column prop="id" :label="$t('generate.schema.structure.operate')">
         <template #default="scope">
           <el-button type="danger" :icon="Delete" @click="deleteField(scope.row.field)" size="small" />
         </template>
