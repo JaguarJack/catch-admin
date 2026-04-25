b4cca563e8d55a730edb7f799247665dc2019768
jaguarjack <njphper@gmail.com>
2026-04-25 08:19:45 +0800

    新增权限模块安装器

---
+++ modules/Permissions/Installer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Permissions;
+
+use Catch\Support\Module\Installer as ModuleInstaller;
+use Modules\Permissions\Providers\PermissionsServiceProvider;
+
+class Installer extends ModuleInstaller
+{
+    protected function info(): array
+    {
+        // TODO: Implement info() method.
+        return [
+            'title' => '权限管理',
+            'name' => 'permissions',
+            'path' => 'permissions',
+            'keywords' => '权限, 角色, 部门',
+            'description' => '权限管理模块',
+            'provider' => PermissionsServiceProvider::class,
+        ];
+    }
+
+    protected function requirePackages(): void
+    {
+        // TODO: Implement requirePackages() method.
+    }
+
+    protected function removePackages(): void
+    {
+        // TODO: Implement removePackages() method.
+    }
+}
