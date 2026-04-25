812d5c0259709a70f858fa11fa2396194b425c58
jaguarjack <njphper@gmail.com>
2026-04-25 08:19:53 +0800

    新增系统模块安装器

---
+++ modules/System/Installer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\System;
+
+use Catch\Support\Module\Installer as ModuleInstaller;
+use Modules\System\Providers\SystemServiceProvider;
+
+class Installer extends ModuleInstaller
+{
+    protected function info(): array
+    {
+        // TODO: Implement info() method.
+        return [
+            'title' => '系统管理',
+            'name' => 'system',
+            'path' => 'system',
+            'keywords' => '系统管理',
+            'description' => '系统管理模块',
+            'provider' => SystemServiceProvider::class,
+        ];
+    }
+
+    protected function requirePackages(): void
+    {
+        // TODO: Implement requirePackages() method.
+    }
+
+    protected function removePackages(): void
+    {
+        // TODO: Implement removePackages() method.
+    }
+}
